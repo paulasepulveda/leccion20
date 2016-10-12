4bd5aef0d262872288f85e66ca7777616cf929a9
Paula Sepulveda <psepulveda26@gmail.com>
2016-10-12 17:11:49 -0300

    ejercicio 1 listo en el archivo js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-// tu codigo va aca
+//ejercicio 1
+function Promedio (a, b, c, callback) {
+	var resultado = (a+b+c)/3;
+
+	return callback (resultado);
+	}
+// ejercicio 2
+var myVar = setInterval(function(){ myTimer() }, 1000);
+
+		function myTimer() {
+    var d = new Date();
+    var t = d.toLocaleTimeString();
+    document.getElementById("demo").innerHTML = 5;
